a6a72a3a0bae2f30af38fcb0c4b61773b343ff84
aladdin65 <thomas_dirkse@hotmail.com>
2015-04-30 15:37:44 +0000

    1eversie

---
+++ Opdracht2/Sieve_of_E.py
@@ -0,0 +1,21 @@
+import math
+import sys
+import time
+N = int(sys.argv[1])
+bestandsNaam = str(sys.argv[2])
+file = open(bestandsNaam, 'w')
+lijst = [x for x in range(0, N + 2)]
+T1 = time.perf_counter()
+for i in range(2, int(math.ceil(math.sqrt(N)))):
+    if lijst[i] != 0:
+        for j in range(2, math.ceil(N // i) + 1):
+            lijst[j * i] = 0
+count = 0
+for i in range(2, N):
+    if lijst[i] != 0:
+        file.write(str(lijst[i]) + "\n")
+        count = count + 1
+T2 = time.perf_counter()
+timeElapsed = T2 - T1
+file.close
+print("Found", count, "Prime numbers smaller than", N, "in", timeElapsed)
