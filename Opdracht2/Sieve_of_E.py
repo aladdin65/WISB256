e267575f4bde2446be75316661fc9cd5f54a66fe
aladdin65 <thomas_dirkse@hotmail.com>
2015-04-30 16:02:56 +0000

    2eVersie

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -4,16 +4,16 @@ import time
 N = int(sys.argv[1])
 bestandsNaam = str(sys.argv[2])
 file = open(bestandsNaam, 'w')
-lijst = [x for x in range(0, N + 2)]
+lijst = [True for x in range(0, N + 2)]
 T1 = time.perf_counter()
 for i in range(2, int(math.ceil(math.sqrt(N)))):
-    if lijst[i] != 0:
+    if lijst[i] != False:
         for j in range(2, math.ceil(N // i) + 1):
-            lijst[j * i] = 0
+            lijst[j * i] = False
 count = 0
 for i in range(2, N):
-    if lijst[i] != 0:
-        file.write(str(lijst[i]) + "\n")
+    if lijst[i] != False:
+        file.write(str(i) + "\n")
         count = count + 1
 T2 = time.perf_counter()
 timeElapsed = T2 - T1
