892a242f8f783b11b80b6aae673cde8fa512ea56
aladdin65 <thomas_dirkse@hotmail.com>
2015-05-06 14:59:22 +0000

    1rst version

---
+++ Opdracht3/estimate_pi.py
@@ -0,0 +1,39 @@
+import sys
+import random
+import math
+N = 0
+L = 0
+seed = 0
+
+try:
+    N = int(sys.argv[1])
+    L = float(sys.argv[2])
+except:
+    print("Use: estimate_pi.py N L")
+    sys.exit()
+try:
+    seed = int(sys.argv[3])
+    random.seed(seed)
+except:
+    seed = 0
+
+def drop_needle(L):
+    x1 = random.random()
+    a = random.vonmisesvariate(0, 0)
+    x2 = x1 + L * math.cos(a)
+    if (x2 <= 0 and x1 >= 0) or (x1 <= 1 and x2 >= 1): return True
+    else: return False
+
+count = 0
+for i in range(0, N):
+    if (drop_needle(L) == True): count = count + 1
+if (count > 0):
+    frac = count / N
+    if (L <= 1):
+        Pi =  2 * L / frac
+    else:
+        Pi = (2 * L) / (frac - 1) - (2 / (frac - 1)) * (math.sqrt(L ** 2 - 1) + math.asin(1 / L))
+else: Pi = 0
+
+print(count, "hits in", N, "tries")
+print(str(Pi))
